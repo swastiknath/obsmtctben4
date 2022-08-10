77f1335d835d92482aa28294b14f453c3647f8f6
Swastik Nath <swastiknath@outlook.com>
2022-08-10 05:42:40 +0530

    Fixed the issue of Improper Title at the Browser History.

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'The Observation MTC TBE 4',
       theme: ThemeData(
         // This is the theme of your application.
         //
